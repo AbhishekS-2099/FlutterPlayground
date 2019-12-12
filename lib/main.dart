6492565f06be29d772bdb54ad9d4e19168e7683f
AbhishekS-2099 <abhishek.sankar.in@gmail.com>
2019-12-12 22:40:41 +0530

    Finished Transition of pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,103 +2,59 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+//import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: Page1(),
+));
+class Page1 extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Animation"),),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go"),
+          onPressed: (){
+            Navigator.of(context).push(_createRoute());
+          },
+        ) ,
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.greenAccent,
       ),
-      home: RandomWords(),
     );
   }
 }
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
+Route _createRoute(){
+  return PageRouteBuilder(
+    pageBuilder: (context,animation,secondaryAnimation)=>Page2(),
+      transitionsBuilder: (context, animation,secondaryAnimation,child){
+        var begin = Offset(0.0,1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var curvedAnimation = CurvedAnimation(
+          parent: animation,
+          curve: curve
+        );
+        return SlideTransition(
+          position: tween.animate(curvedAnimation),
+          child: child,
+        );
+      }
+  );
 }
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
+class Page2 extends StatelessWidget{
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Startup Name Generator"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _pushSaved,
-          )
-        ],
+        title: Text("You're now in Page 2"),
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
-        return ListTile(
-          title: Text(
-            pair.asPascalCase,
-            style: _biggerFont,
-          ),
-        );
-      });
-      final List<Widget> divided =
-          ListTile.divideTiles(context: context, tiles: tiles).toList();
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Saved Suggestions"),
-        ),
-        body: ListView(
-          children: divided,
-        ),
-      );
-    }));
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(pair.asPascalCase, style: _biggerFont),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
+      body: Center(
+        child: Text("Yay, Transition Success"),
       ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
   }
 }
+
